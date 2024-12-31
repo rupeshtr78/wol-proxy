bf37bcc9026c5073173cde78e453412d657f917d
rupeshtr78 <rupeshtr78@gmail.com>
2024-12-31 14:48:06 -0800

    Update cookie and curl scripts for WOL configuration and port adjustment

--- scripts/curl.sh
+++ scripts/curl.sh
@@ -4,7 +4,7 @@ MAC_ADDR="F4:93:9F:F4:04:5B"
 BIND_ADDR="0.0"
 BROADCAST_ADDR=""
 
-curl -X POST http://127.0.0.1:8090/wol \
+curl -X POST http://10.0.0.213:8090/wol \
 -H "Content-Type: application/json" \
 -d '{"mac_address": "'$MAC_ADDR'", "bind_address": "'$BIND_ADDR'", "broadcast_address": "'$BROADCAST_ADDR'"}' \
--b "wol-cookie=we12come.561dac11efb0bbdb716194726ca9cae1170cd72113248364e176b37954a16f84"
+-b "wol-cookie=we12come.52fd317cfe9453f8a3fc425fabfd83fd6523a2489cf8d5ec9b73085fc19018ba"
