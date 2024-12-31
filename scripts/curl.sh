cf6e4071b5f49b7bba2ea599a1db06f75f6a8fc9
rupeshtr78 <rupeshtr78@gmail.com>
2024-12-30 23:11:01 -0800

    Add actix-web-httpauth dependency and update server port configuration

--- scripts/curl.sh
+++ scripts/curl.sh
@@ -4,6 +4,6 @@ MAC_ADDR="F4:93:9F:F4:04:5B"
 BIND_ADDR="0.0"
 BROADCAST_ADDR=""
 
-curl -X POST http://127.0.0.1:8080/wol \
+curl -X POST http://127.0.0.1:8090/wol \
 -H "Content-Type: application/json" \
 -d '{"mac_address": "'$MAC_ADDR'", "bind_address": "'$BIND_ADDR'", "broadcast_address": "'$BROADCAST_ADDR'"}'
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 use ::actix_web::http::StatusCode;
-use wol::WolRequest;
-mod wol;
 use actix_web::{middleware, web, App, HttpResponse, HttpServer, Responder};
 use log;
+use wol::WolRequest;
+mod wol;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
     log::info!("starting HTTP server at http://localhost:8080");
+
+    let port = std::env::var("WOL_PORT").unwrap_or("8090".to_string());
 
     // start http server with actix-web
     HttpServer::new(|| {
@@ -15,7 +17,7 @@ async fn main() -> std::io::Result<()> {
             .wrap(middleware::Logger::default())
             .route("/wol", web::post().to(send_wol_request))
     })
-    .bind("10.0.0.191:8090")?
+    .bind(format!("0.0.0.0:{}", port))?
     .run()
     .await
 }
@@ -38,21 +40,4 @@ async fn send_wol_request(wol_req: web::Json<WolRequest>) -> impl Responder {
             StatusCode::OK
         )),
     }
-}
-
-fn check_wol() {
-    // let request = WolRequest::new("F4:93:9F:F4:04:5B".to_string());
-    let request = WolRequest {
-        mac_address: "F4:93:9F:F4:04:5B".to_string(),
-        bind_addr: Some("0.0.0.0".to_string()),
-        broadcast_addr: Some("255.255.255.255".to_string()),
-    };
-    let result = wol::send_wol(&request);
-    match result {
-        Err(e) => eprintln!("Error: {}", e),
-        Ok(_) => println!("Magic packet sent successfully"),
-    }
-
-    // call wol::send_wol as actix-web post request
-    // the post request should take a json object with WolRequest fields
 }
