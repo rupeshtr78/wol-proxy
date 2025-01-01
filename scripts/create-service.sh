94d5ae1a0e1e82271cf27a236d09074c59513eb5
rupeshtr78 <rupeshtr78@gmail.com>
2025-01-01 12:57:43 -0800

    Add script to create and manage wol-proxy systemd service

---
+++ scripts/create-service.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Create wol-proxy service
+# replace the path with the actual path of the binary file
+# replace the user with the actual user
+# replace environment variables with the actual values
+sudo mv scripts/wol-proxy.service /etc/systemd/system/wol-proxy.service
+
+# Reload systemd manager configuration
+sudo systemctl daemon-reload
+sudo systemctl enable wol-proxy.service
+sudo systemctl start wol-proxy.service
