1d11238b5005bd7e42967f819cbbd2b76b3dd1a3
rupeshtr78 <rupeshtr78@gmail.com>
2025-01-02 08:53:00 -0800

    Update

--- scripts/curl.sh
+++ scripts/curl.sh
@@ -7,4 +7,4 @@ BROADCAST_ADDR=""
 curl -X POST http://10.0.0.42:9080/wol \
 -H "Content-Type: application/json" \
 -d '{"mac_address": "'$MAC_ADDR'", "bind_address": "'$BIND_ADDR'", "broadcast_address": "'$BROADCAST_ADDR'"}' \
--b "wol-cookie=we12come.39a64a52139d4d95cd0f46982a04e4a0b40bd22930c780a0edbe3ee06c51096c"
+-b "cookie_name:cookie_val"
