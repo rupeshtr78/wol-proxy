3bb2af96cf75bfb0d9adca9f584205b5c4dcfe59
rupeshtr78 <rupeshtr78@gmail.com>
2025-01-12 15:55:14 -0800

    curl scripts

--- scripts/curl.sh
+++ scripts/curl.sh
@@ -3,8 +3,6 @@
 MAC_ADDR="F4:93:9F:F4:04:5B"
 BIND_ADDR="0.0"
 BROADCAST_ADDR=""
-COOKIE_NAME=wol-cookie
-COOKIE=wol-cookie=wol-proxy.23664c370774284a7620cb511bef8892e49b844cc5e8725ed8e7af7e82719c34
 
 if WOL_TLS == "false" then 
     curl -X POST http://10.0.0.42:9080/wol \
